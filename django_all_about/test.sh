b07a930b83428fa3eb5b364d22b72ea89910dd17
Nuung <qlgks1@naver.com>
2023-01-19 01:28:53 +0900

    test

--- django_all_about/test.sh
+++ django_all_about/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f ./django_all_about/requirements.txt ]; then
-    echo "requirements.txt exists!"
+    echo "SUCCESS: requirements.txt exists!"
 else
     echo "FAILURE: requirements.txt does not exist!"
     exit 1
