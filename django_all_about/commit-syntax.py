e7404f36ac3b62c9075ecb903dbc151973199a5d
Nuung <qlgks1@naver.com>
2023-01-19 02:04:41 +0900

    update: pre commit update

--- django_all_about/commit-syntax.py
+++
@@ -1,9 +0,0 @@
-import re
-import sys
-import pathlib
-import argparse
-
-COMMIT_SYNTAX = re.compile(r"^(update\:)|^(create\:)|^(delete\:)|^(hotfix\:)")
-
-print(argparse.ArgumentParser())
-sys.exit()
