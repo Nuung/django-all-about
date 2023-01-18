b1953405c272f69f1df38b7326541f214f772cc3
Nuung <qlgks1@naver.com>
2023-01-19 01:57:30 +0900

    temp

---
+++ django_all_about/commit-syntax.py
@@ -0,0 +1,13 @@
+import re
+import pathlib
+import argparse
+
+COMMIT_SYNTAX = re.compile(r"^(update\:)|^(create\:)|^(delete\:)|^(hotfix\:)")
+FAIL = 1
+SUCCESS = 0
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    print(parser)
+    return SUCCESS
