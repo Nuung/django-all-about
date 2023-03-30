3d4d20d3744f4c646ac237abf7d5cada3a4dedd5
Nuung <qlgks1@naver.com>
2023-03-31 02:36:21 +0900

    feature: django model manager and mixin pattern test

--- django_all_about/apis/test/models.py
+++ django_all_about/apis/test/models.py
@@ -1,7 +1,15 @@
+import datetime
+
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 from apis.user.models import User
+
+
+class CrnOpenedManager(models.Manager):
+    def all_closed(self):
+        return self.get_queryset().filter(is_closed=False)
 
 
 class CheckedCrn(models.Model):
@@ -11,6 +19,8 @@ class CheckedCrn(models.Model):
     is_closed = models.BooleanField(blank=False, null=False, verbose_name="사업자 폐업여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CrnOpenedManager()
 
     def __str__(self) -> str:
         return f"{self.registration_number}: {'closed' if self.is_closed else 'not close'} "
@@ -49,3 +59,65 @@ class Cart(models.Model):
 
     def __str__(self) -> str:
         return f"{self.pk} {self.user.name}, {self.user.email} 유저가 {self.product} 담음"
+
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SlugMixin(models.Model):
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if hasattr(self, "title") and not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+class PostStatus(models.TextChoices):
+    DEFAULT = "DE", _("Default")
+    PUBLIC = "PU", _("Public")
+    PRIVATE = "PR", _("Private")
+
+
+class PublicPostMixin:
+    def pre(self):
+        if self.status is not PostStatus.PUBLIC:
+            return
+
+    def set_public(self):
+        self.status = PostStatus.PUBLIC.value
+        self.save()
+
+
+class PrivatePostMixin:
+    def set_private(self):
+        self.status = PostStatus.PRIVATE.value
+        self.save()
+
+
+class Post(TimestampMixin, SlugMixin, PublicPostMixin, PrivatePostMixin):
+    title = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=2,
+        choices=PostStatus.choices,
+        default=PostStatus.DEFAULT,
+    )
+    body = models.TextField()
+    objects = models.Manager()
+
+    @property
+    def is_old(self):
+        if datetime.datetime.now().day - self.created_at.day > 1:
+            return True
+        return False
+
+    def __str__(self):
+        return self.title
