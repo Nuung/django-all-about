5a16934d2ecc53a154658edb705f6a36b480fc14
Nuung <qlgks1@naver.com>
2023-08-17 00:27:36 +0900

    modify: test code 디렉토리 분리

--- django_all_about/config/dbrouter.py
+++ django_all_about/config/dbrouter.py
@@ -10,14 +10,14 @@ class MultiDBRouter:
         """
         return "read"
 
-    def db_for_write(self, model, **hints):
-        # "insert", "update", "delete" 작업은 "default" 데이터베이스로 라우팅
-        return "default"
+    # def db_for_write(self, model, **hints):
+    #     # "insert", "update", "delete" 작업은 "default" 데이터베이스로 라우팅
+    #     return "default"
 
-    def allow_relation(self, obj1, obj2, **hints):
-        # 관계 허용
-        return True
+    # def allow_relation(self, obj1, obj2, **hints):
+    #     # 관계 허용
+    #     return True
 
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        # 마이그레이션 허용
-        return True
+    # def allow_migrate(self, db, app_label, model_name=None, **hints):
+    #     # 마이그레이션 허용
+    #     return True
